9d1c0a67c023a55a2451b7c69647fd0df3ce2ee2
kennyChukwuebuka <kenkinado@gmail>
2023-10-21 13:46:06 +0100

    knuth techique

---
+++ 100-shell_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts array of integers in 
+ * ascending order using shell sort technique.
+ * @array: Array to be sorted.
+ * @size: Size of the array.
+ * Return: Sorted arrays
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	int a, b, k, l, knuth,tmp;
+
+	if (!array)
+		return;
+
+	l = (int)size;
+	for (k = 1; k < l; k = (k * 3) + 1)
+	{
+		knuth = k;
+	}
+
+	for (k = knuth; k > 0; k = (k - 1) / 3)
+	{
+		for (a = k; a < l; a++)
+		{
+			tmp = array[a];
+
+			for (b = a; b >= k && array[b - k] > tmp; b -= k)
+			{
+				array[b] = array[b - k];
+			}
+			array[b] = tmp;
+		}
+		print_array(array, size);
+	}
+}
